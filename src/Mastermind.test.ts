708490b9cee122be51e452caf3926dd50eaaf927
Shigoto-dev19 <shigoto-dev19@protonmail.com>
2024-03-25 03:08:51 +0100

    Add pseudo-code for Mastermind Zkapp integration tests

--- src/Mastermind.test.ts
+++ src/Mastermind.test.ts
@@ -1,67 +1,145 @@
-import { MastermindZkApp } from './Mastermind';
-import { Field, Mina, PrivateKey, PublicKey, AccountUpdate } from 'o1js';
+//TODO Add utility functions for serialization and game mechanics
+//TODO Add unit tests for game mechanics
 
-/*
- * This file specifies how to test the `Add` example smart contract. It is safe to delete this file and replace
- * with your own tests.
- *
- * See https://docs.minaprotocol.com/zkapps for more info.
- */
+import { MastermindZkApp } from './Mastermind';
+import {
+  Field,
+  Mina,
+  PrivateKey,
+  PublicKey,
+  AccountUpdate,
+  MerkleTree,
+  UInt8,
+} from 'o1js';
 
 let proofsEnabled = false;
 
-describe('Add', () => {
-  let deployerAccount: PublicKey,
-    deployerKey: PrivateKey,
-    senderAccount: PublicKey,
-    senderKey: PrivateKey,
-    zkAppAddress: PublicKey,
-    zkAppPrivateKey: PrivateKey,
-    zkApp: MastermindZkApp;
+async function localDeploy(
+  zkapp: MastermindZkApp,
+  deployerKey: PrivateKey,
+  zkappPrivateKey: PrivateKey
+) {
+  const deployerAccount = deployerKey.toPublicKey();
+  const tx = await Mina.transaction(deployerAccount, () => {
+    AccountUpdate.fundNewAccount(deployerAccount);
+    zkapp.deploy();
+  });
+
+  await tx.prove();
+  await tx.sign([deployerKey, zkappPrivateKey]).send();
+}
+
+async function initializeGame(zkapp: MastermindZkApp, deployerKey: PrivateKey) {
+  const deployerAccount = deployerKey.toPublicKey();
+
+  // The deployer initializes the Mastermind zkapp
+  const initTx = await Mina.transaction(deployerAccount, () => {
+    zkapp.initGame();
+  });
+
+  await initTx.prove();
+  await initTx.sign([deployerKey]).send();
+}
+
+describe('Mastermind ZkApp Tests', () => {
+  let codemasterKey: PrivateKey,
+    codebreakerKey: PrivateKey,
+    intruderKey: PrivateKey,
+    zkappAddress: PublicKey,
+    zkappPrivateKey: PrivateKey,
+    zkapp: MastermindZkApp,
+    guessTree: MerkleTree,
+    clueTree: MerkleTree,
+    secretCombination: number[],
+    codemasterSalt: Field,
+    codebreakerSalt: Field;
 
   beforeAll(async () => {
     if (proofsEnabled) await MastermindZkApp.compile();
-  });
 
-  beforeEach(() => {
+    // Set up the Mina local blockchain
     const Local = Mina.LocalBlockchain({ proofsEnabled });
     Mina.setActiveInstance(Local);
-    ({ privateKey: deployerKey, publicKey: deployerAccount } =
-      Local.testAccounts[0]);
-    ({ privateKey: senderKey, publicKey: senderAccount } =
-      Local.testAccounts[1]);
-    zkAppPrivateKey = PrivateKey.random();
-    zkAppAddress = zkAppPrivateKey.toPublicKey();
-    zkApp = new MastermindZkApp(zkAppAddress);
+
+    // Local.testAccounts is an array of 10 test accounts that have been pre-filled with Mina
+    codemasterKey = Local.testAccounts[0].privateKey;
+    codebreakerKey = Local.testAccounts[1].privateKey;
+    intruderKey = Local.testAccounts[2].privateKey;
+
+    // Set up the zkapp account
+    zkappPrivateKey = PrivateKey.random();
+    zkappAddress = zkappPrivateKey.toPublicKey();
+    zkapp = new MastermindZkApp(zkappAddress);
+
+    // Initialize the off-chain Merkle Tree for guess & clue storage respectively
+    guessTree = new MerkleTree(8);
+    clueTree = new MerkleTree(8);
+
+    // Generate random field as salt for the codemaster & codebreaker respectively
+    codemasterSalt = Field.random();
+    codebreakerSalt = Field.random();
+
+    // Set up a valid secret combinartion for testing purposes
+    // Better encode the colours as number or enums locally
+    secretCombination = [];
   });
 
-  async function localDeploy() {
-    const txn = await Mina.transaction(deployerAccount, () => {
-      AccountUpdate.fundNewAccount(deployerAccount);
-      zkApp.deploy();
+  describe('Deploy and initialize Mastermind zkApp', () => {
+    it('Generate and Deploy `Mastermind` smart contract', async () => {
+      await localDeploy(zkapp, codemasterKey, zkappPrivateKey);
     });
-    await txn.prove();
-    // this tx needs .sign(), because `deploy()` adds an account update that requires signature authorization
-    await txn.sign([deployerKey, zkAppPrivateKey]).send();
-  }
 
-  it('generates and deploys the `Add` smart contract', async () => {
-    await localDeploy();
-    const num = zkApp.num.get();
-    expect(num).toEqual(Field(1));
+    // This is test verifies that the zkapp initial state values are correctly set up
+    it('Initialize game', async () => {
+      await initializeGame(zkapp, codemasterKey);
+
+      const codemasterId = zkapp.codemasterId.get();
+      expect(codemasterId).toEqual(Field(0));
+
+      const codebreakerId = zkapp.codebreakerId.get();
+      expect(codebreakerId).toEqual(Field(0));
+
+      const turnCount = zkapp.turnCount.get();
+      expect(turnCount).toEqual(new UInt8(0));
+
+      const solutionHash = zkapp.solutionHash.get();
+      expect(solutionHash).toEqual(Field(0));
+
+      const serializedGuess = zkapp.serializedGuess.get();
+      expect(serializedGuess).toEqual(Field(0));
+
+      const serializedClue = zkapp.serializedClue.get();
+      expect(serializedClue).toEqual(Field(0));
+    });
   });
 
-  it('correctly updates the num state on the `Add` smart contract', async () => {
-    await localDeploy();
+  describe('createGame method tests', () => {
+    it.todo('should reject codemaster with invalid secret combination');
+    it.todo('should create a game and update codemasterId');
+    it.todo('should prevent other players from re-creating a game');
+  });
 
-    // update transaction
-    const txn = await Mina.transaction(senderAccount, () => {
-      zkApp.update();
-    });
-    await txn.prove();
-    await txn.sign([senderKey]).send();
+  describe('makeGuess method tests', () => {
+    it.todo('should reject codebreaker to call this method before game is created');
+    it.todo('should reject codebreaker to make an invalid guess');
+    it.todo('should make valid guess & update state');
+    it.todo('should reject the codebraker calling this method if the clue from previous turn is not reported');
 
-    const updatedNum = zkApp.num.get();
-    expect(updatedNum).toEqual(Field(3));
+    //! this is import to test the rest of method apart from first turn
+    //! the giveClue is tested separately
+    it.todo('should accept codemaster clue');
+
+    it.todo('should reject any caller other than the codebreaker');
+    it.todo('should reject a codebreaker with different salt');
+    it.todo('should accept another valid guess from the codebreaker');
+  });
+
+  describe('giveClue method tests', () => {
+    it.todo('should reject any caller other than the codemaster');
+    it.todo('should reject codemaster with non-compliant secret combination');
+    it.todo('should reject codemaster with different salt');
+    it.todo('should accept clue TX and update state');
+
+    //TODO repeat the cycle few 10 times more and add win-lose case tests
   });
 });
